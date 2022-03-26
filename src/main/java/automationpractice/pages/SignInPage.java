5e6db6ea31723f5fa1c19256a122adc92bbbcd17
Vijay <vijaythakar777@gmail.com>
2022-03-26 16:10:44 -0400

    Second commit

--- src/main/java/automationpractice/pages/SignInPage.java
+++ src/main/java/automationpractice/pages/SignInPage.java
@@ -14,34 +14,54 @@ public class SignInPage extends TestBase {
 	// for login Button
 	@FindBy(className = "login")
 	WebElement loginbutton;
-
+	
+	
+	//for email address field
 	@FindBy(id = "email")
 	WebElement username;
-
+	
+	//for password field
 	@FindBy(name = "passwd")
 	WebElement password;
-
+	
+	//for submit button
 	@FindBy(name = "SubmitLogin")
 	WebElement signInbutton;
-
+	
+	//to check header value on login page
 	@FindBy(xpath="//h1[contains(text(),'Authentication')]")
 	WebElement header;
 	
+	//when passing only password
 	@FindBy(xpath="//li[contains(text(),'An email address required.')]")
 	WebElement UserNameError;
 	
+	//When passing wrong email
+	@FindBy(xpath="//li[contains(text(),'Invalid email address.')]")
+	WebElement invalidEmail;
+	
+	//When passing validEmail and wrong password
+	@FindBy(xpath="//li[contains(text(),'Authentication failed.')]")
+	WebElement authenticationFailed;
+	
+	
+	//When passing only username
 	@FindBy(xpath="//li[contains(text(),'Password is required.')]")
 	WebElement PasswordError;
 	
+	//For forgot password link
 	@FindBy(xpath="//a[contains(text(),'Forgot your password?')]")
 	WebElement forgotPasswordLink;
 	
+	//for retrieve password button
 	@FindBy(xpath="//span[contains(text(),'Retrieve Password')]")
 	WebElement retrievePassword;
 	
+	//For confirm text after clicking retrivepassword button
 	@FindBy(css="p[class*='alert-success']")
 	WebElement confirmText;
 	
+	//when passing invalid email in forgot password field
 	@FindBy(xpath="//li[contains(text(),'Invalid email address.')]")
 	WebElement invalidEmailInForgotPassword;
 	
@@ -80,6 +100,7 @@ public class SignInPage extends TestBase {
 		
 	}
 	
+	//for username error text
 	public String readUsernameError()
 	{
 		String actualError = UserNameError.getText();
@@ -87,6 +108,7 @@ public class SignInPage extends TestBase {
 		return actualError;
 	}
 	
+	//for password error text
 	public String readPasswordError()
 	{
 		String actualError = PasswordError.getText();
@@ -94,6 +116,23 @@ public class SignInPage extends TestBase {
 		return actualError;
 	}
 	
+	//for invalid authentication error // when passing valid email and wrong password
+	public String reaadAuthenticationError()
+	{
+		String authError = authenticationFailed.getText();
+		System.out.println(authError);
+		return authError;
+	}
+	
+	// For invalid email address // when passing invalid email and correct password
+	public String readInvalidEmailAddressInLogin()
+	{
+		String invalidEmailInLogin = invalidEmail.getText();
+		System.out.println(invalidEmailInLogin);
+		return invalidEmailInLogin;
+	}
+	
+	//to read confirmation email text after passing valid email in forgot password page
 	public String readConfirmationEmailText()
 	{
 		String successText = confirmText.getText();
@@ -101,13 +140,54 @@ public class SignInPage extends TestBase {
 		return successText;
 	}
 	
-	
+	//to read invalid email in forgot password page
 	public String invalidEmailInForgotPassword()
 	{
 		String invalidEmail = invalidEmailInForgotPassword.getText();
 		System.out.println(invalidEmail);
 		return invalidEmail;
 	}
+	
+	//Giving String as Email Address without any @ sign
+	public void StringAsEmailAddress(String uname, String pswd)
+	{
+		username.sendKeys(uname);
+		password.sendKeys(pswd);
+		signInbutton.click();
+	}
+	
+	//Passing number as a Email address
+	public void numberAsEmailAddress(String uname, String pswd)
+	{
+		username.sendKeys(uname);
+		password.sendKeys(pswd);
+		signInbutton.click();
+	}
+	
+	//passing only special characters as a Email address
+	public void onlySpecialCharacterAsEmailAddress(String uname, String pswd)
+	{
+		username.sendKeys(uname);
+		password.sendKeys(pswd);
+		signInbutton.click();
+	}
+	
+	//Passing email value without dot
+	public void missingDotAsEmailAddress(String uname, String pswd)
+	{
+		username.sendKeys(uname);
+		password.sendKeys(pswd);
+		signInbutton.click();
+	}
+	
+	//passing 2 @ sign as email address
+	public void twoAtSignsAsEmailAddress(String uname, String pswd)
+	{
+		username.sendKeys(uname);
+		password.sendKeys(pswd);
+		signInbutton.click();
+	}
+	
 	
 	public void validForgotPasswordFunction(String uname)
 	{
--- src/test/java/automationpractice/testcases/SignInPageTest.java
+++ src/test/java/automationpractice/testcases/SignInPageTest.java
@@ -29,66 +29,223 @@ public class SignInPageTest extends TestBase {
 
 	}
 
-	@Test(priority=1)
-	public void SignInPageTitleTest() {
+	// For checking log in page title
+	@Test(priority = 1)
+	public void signInPageTitleTest() {
 		first.SignInClick();
 		String title = signInPage.validateLoginPageTitle();
 		Assert.assertEquals(title, "Login - My Store");
 	}
 
-	@Test(priority=2)
-	public void SignInPageHeaderTest() {
+	// For checking Header in Log in Page to make sure we are at right page
+	@Test(priority = 2)
+	public void signInPageHeaderTest() {
 		first.SignInClick();
 		boolean header = signInPage.validateHeader();
 		Assert.assertTrue(header);
 	}
 
-	@Test(priority=3)
-	public void ValidloginTest() {
+	// For valid log in functionality
+	@Test(priority = 3)
+	public void validloginTest() {
 		first.SignInClick();
-		homePage = signInPage.login(prop.getProperty("username"), prop.getProperty("password")); // this login is
-																									// returning home
-																									// page class so we
-																									// saved as homePage
-
+		// this login is returning homepage class so we saved as homePage
+		homePage = signInPage.login(prop.getProperty("username"), prop.getProperty("password"));
 	}
 
-	@Test(priority=4)
-	public void InvalidUsernameloginTest() {
+	// For empty user test with valid password
+	@Test(priority = 4)
+	public void emptyUsernameWithValidPasswordloginTest() {
 		first.SignInClick();
 		homePage = signInPage.login(prop.getProperty("emptyUsername"), prop.getProperty("password"));
 		Assert.assertEquals(signInPage.readUsernameError(), "An email address required.");
 	}
 
-	@Test(priority=5)
-	public void InvalidPasswordloginTest() {
+	// for Empty username with Invalid password
+	@Test(priority = 5)
+	public void emptyUsernameWithInvalidPasswordloginTest() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("emptyUsername"), prop.getProperty("invalidPassword"));
+		Assert.assertEquals(signInPage.readUsernameError(), "An email address required.");
+	}
+
+	// for unregistered email with registered password
+	@Test(priority = 6)
+	public void unregisteredUsernameWithValidPasswordLoginTest() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("unregisteredUsername"), prop.getProperty("password"));
+		Assert.assertEquals(signInPage.reaadAuthenticationError(), "Authentication failed.");
+	}
+
+	// for unregistered email with unregistered password
+	@Test(priority = 7)
+	public void unregisteredUsernameWithUnregisteredPasswordLoginTest() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("unregisteredUsername"), prop.getProperty("invalidPassword"));
+		Assert.assertEquals(signInPage.reaadAuthenticationError(), "Authentication failed.");
+	}
+
+	// For valid username and empty password
+	@Test(priority = 8)
+	public void invalidPasswordWithValidUsernameloginTest() {
 		first.SignInClick();
 		homePage = signInPage.login(prop.getProperty("username"), prop.getProperty("emptyPassword"));
 		Assert.assertEquals(signInPage.readPasswordError(), "Password is required.");
 	}
 
-	@Test(priority = 6)
-	public void InvalidCredentialsloginTest() {
+	// fir both empty username and password
+	@Test(priority = 9)
+	public void invalidCredentialsloginTest() {
 		first.SignInClick();
 		homePage = signInPage.login(prop.getProperty("emptyUsername"), prop.getProperty("emptyPassword"));
 		Assert.assertEquals(signInPage.readUsernameError(), "An email address required.");
 	}
-	
-	@Test(priority=7)
-	public void validForgotPasswordLinkFunction() 
-	{
+
+	// Test for Passing String as Email Address(Invalid Email) and valid password
+	@Test(priority = 10)
+	public void stringAsUsernameWithValidPasswordTest() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("stringAsUsername"), prop.getProperty("password"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+
+	}
+
+	// Test For passing String as Email Address(Invalid Email) and empty password
+	@Test(priority = 11)
+	public void stringAsUsernameWithEmptyPasswordTest() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("stringAsUsername"), prop.getProperty("emptyPassword"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+	}
+
+	// Test for passing String as Email Address(Invalid Email) and invalid password
+	@Test(priority = 12)
+	public void stringAsUsernameWithInvalidPasswordTest() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("stringAsUsername"), prop.getProperty("invalidPassword"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+	}
+
+	// Test for passing number as Email address(Invalid Email) and valid password
+	@Test(priority = 13)
+	public void numberAsUsernameWithValidPassword() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("numbersAsUsername"), prop.getProperty("password"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+
+	}
+
+	// Test for passing number as Email address(Invalid Email) with empty password
+	@Test(priority = 14)
+	public void numberAsUsernameWithEmptyPassword() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("numbersAsUsername"), prop.getProperty("emptyPassword"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+	}
+
+	// Test for passing number as Email Address(Invalid Email) with Invalid Password
+	@Test(priority = 15)
+	public void numberAsUsernameWithInvalidPassword() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("numbersAsUsername"), prop.getProperty("invalidPassword"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+	}
+
+	// Test for only Special Character as Email Address(Invalid Email) with valid
+	// password
+	@Test(priority = 16)
+	public void specialCharAsUsernameWithValidPassword() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("onlySpecialCharacterAsUsername"), prop.getProperty("password"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+
+	}
+
+	// Test for only special Characters as Email Address(Invalid Email) with empty
+	// password
+	@Test(priority = 17)
+	public void specialCharAsUsernameWithEmptyPassword() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("onlySpecialCharacterAsUsername"),
+				prop.getProperty("emptyPassword"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+	}
+
+	// Test for only special Characters as Email Address(Invalid Email) with invalid
+	// password
+	@Test(priority = 18)
+	public void specialCharAsUsernameWithInvalidPassword() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("onlySpecialCharacterAsUsername"),
+				prop.getProperty("invalidPassword"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+	}
+
+	// Test for missing . (dot) as Email Address(invalid Email) with valid password
+	@Test(priority = 19)
+	public void missingDotAsEmailWithValidPasswordTest() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("missingDotAsUsername"), prop.getProperty("password"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+	}
+
+	// Test for missing . (dot) as Email Address(Invalid Email) with empty password
+	@Test(priority = 20)
+	public void missingDotAsEmailWithEmptyPasswordTest() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("missingDotAsUsername"), prop.getProperty("emptyPassword"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+	}
+
+	// Test for missing . (dot) as Email Address(Invalid Email) with invalid
+	// password
+	@Test(priority = 21)
+	public void missingDotAsEmailWithInvalidPasswordTest() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("missingDotAsUsername"), prop.getProperty("invalidPassword"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+	}
+
+	// Test for 2 @ sign as Email Address(invalid email) with valid password
+	@Test(priority = 22)
+	public void twoAtSignInEmailWithValidPasswordTest() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("twoAtSignAsUsername"), prop.getProperty("password"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+	}
+
+	// Test for 2 @ sign as Email Address (Invalid Email) with empty password
+	@Test(priority = 23)
+	public void twoAtSignInEmailAddressWithEmptyPassword() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("missingDotAsUsername"), prop.getProperty("emptyPassword"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+	}
+
+	// Test for 2 @ sign as Email Address (Invalid Email) with invalid password
+	@Test(priority = 24)
+	public void twoAtSignInEmailAddressWithInvalidPassword() {
+		first.SignInClick();
+		homePage = signInPage.login(prop.getProperty("missingDotAsUsername"), prop.getProperty("invalidPassword"));
+		Assert.assertEquals(signInPage.readInvalidEmailAddressInLogin(), "Invalid email address.");
+	}
+
+	// Test for performing valid forgot password funtion
+	@Test(priority = 25)
+	public void validForgotPasswordLinkFunction() {
 		first.SignInClick();
 		signInPage.validForgotPasswordFunction(prop.getProperty("username"));
-		Assert.assertEquals(signInPage.readConfirmationEmailText(), "A confirmation email has been sent to your address: " +  prop.getProperty("username") );
+		Assert.assertEquals(signInPage.readConfirmationEmailText(),
+				"A confirmation email has been sent to your address: " + prop.getProperty("username"));
 	}
-	
-	@Test(priority=8)
-	public void invalidForgotPasswordLinkFunction() 
-	{
+
+	// Test for performing invalid forgot password function
+	@Test(priority = 26)
+	public void invalidForgotPasswordLinkFunction() {
 		first.SignInClick();
 		signInPage.validForgotPasswordFunction(prop.getProperty("emptyUsername"));
 		Assert.assertEquals(signInPage.invalidEmailInForgotPassword(), "Invalid email address.");
-	}	
+	}
 
 	@AfterMethod
 	public void tearDown() {
